eaec8aba784c4a93552f61c7059cbf241826eeb5
Billy Farroll <watanight@hotmail.co.uk>
2015-04-06 15:59:53 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,52 @@
+<?php require_once("session.php"); ?>
+<?php require_once("connect.php"); ?>
+<?php require_once("functions.php"); ?>
+<?php include_once("header.php"); ?>
+         
+<link rel="stylesheet" href="css/about.css">
+        <div id="container">
+             <ul class="img-list">
+                
+                 <li>
+                   <img src="images/billy.jpg" height="300px" width="300px" alt="Billy Farroll"/>
+                    <span class="text-content"><span>Billy Farroll<br>Bournemouth University student<br> From Southampton<br> Age: 20 <br> Goals: Game Development </span></span>
+                 </li>
+                 <li>
+                    <img src="images/kt2.png" height="300px" width="300px" alt="Katie"/>
+                    <span class="text-content"><span>Katie Mcgowan<br>Bournemouth University student<br> From Northern Ireland<br> Age:18 <br> Goals: Who even knows at this stage tbh</span></span>
+                 </li>
+                 <li>
+                    <img src="images/bankole.jpg" height="300px" width="300px" alt="Banks"/>
+                    <span class="text-content"><span>Banks<br>Bournemouth University student<br> From Southampton</span></span>
+                 </li>
+                 
+             </ul>
+             
+         </div>
+     </body>
+
+<style>
+body {
+    background-image: url("holiday.jpg");
+}
+</style>
+
+<style>
+
+h1 {
+    text-align: center;
+}
+
+p.date {
+    text-align: right;
+}
+
+p.main {
+    text-align: justify;
+}
+       
+       </style>
+
+<footer class="footer"> <p>Thank you for reading our bios</p></footer>
+
+</html> 
