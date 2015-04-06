2a7dcbc9c6cfe59fdeb7fba1e32612c2e794a052
Billy Farroll <watanight@hotmail.co.uk>
2015-04-06 16:02:55 +0100

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,17 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
+
+
+<div class="navbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="holidays.php">Holidays</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="about-site.php">About The Site</a></li>
+        </ul>
+
+</div>
+
+<?php require_once("../includes/templates/footer.php");?>
