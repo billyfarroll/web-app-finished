983f72e1a91402fd5ce383d48be04007a9432746
Billy Farroll <watanight@hotmail.co.uk>
2015-04-06 16:04:07 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Project</title>
+        <link href="css/normal.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="header">
+    <h2>Website</h2>
+            
+</div> 
+
+<div class="navbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="holidays.php">Holidays</a></li>
+        </ul>
+
+</div>
+    <?php echo message(); ?>
