3b49319b298e6465a219593fd6255bdbfb925776
Billy Farroll <watanight@hotmail.co.uk>
2015-04-06 16:00:22 +0100

    Create about-site.php

---
+++ about-site.php
@@ -0,0 +1,17 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include_once("../includes/templates/header.php"); ?>
+
+<div class="about-site">
+    <p>At HolidayConsultant, we strive to bring you the best new holiday destinations, as chosen by you. Been somewhere nice recently? Share your holiday with the world and let them know they should visit. Been somewhere not so nice? Share that too! With our rating system you can easily inform our other users if you think a destination is worth the trip, or if you'd never return there again. Add a description about your trip in detail, or just say a few words about it, it's completely up to you.</p>
+    <p>HolidayConsultant is still a young website, with much more growing to do. In time the uses will only grow, with future development hopefully allowing the addition of pictures to your review, and a tag system to find only the holidays you're looking for.</p>
+
+
+
+</div>
+
+
+
+        
+<?php require_once("../includes/templates/footer.php");?>   
